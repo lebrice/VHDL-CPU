e827ed357f9872cb2fa2f1d688cf752ea0f53a95
Asher Wright <sastreen@hotmail.com>
2017-02-26 00:12:57 -0500

    Added Port Map for Fetch

--- Deliverable4/processor/fetchStage.vhd
+++ Deliverable4/processor/fetchStage.vhd
@@ -4,12 +4,16 @@ use IEEE.numeric_std.all;
 
 entity fetchStage is
   port (
-    clock : std_logic
+    clock : in std_logic;
+    branchTarget : in std_logic_vector(25 downto 0);
+    branch : in std_logic;
+    disableAdder : in std_logic;
+    instruction : out std_logic_vector(31 downto 0);
+    PCPlus4 : out std_logic_vector(3 downto 0)
   ) ;
 end fetchStage;
 
 architecture fetchStage_arch of fetchStage is
-
 
 
 begin
