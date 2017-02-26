f1e6d1189c20db051bae7d89c55b9db475262b11
Asher Wright <sastreen@hotmail.com>
2017-02-26 00:21:18 -0500

    Added port for memory stage

--- Deliverable4/processor/memoryStage.vhd
+++ Deliverable4/processor/memoryStage.vhd
@@ -5,6 +5,12 @@ use IEEE.numeric_std.all;
 entity memoryStage is
   port (
     clock : in std_logic;
+    ALU_ResultIn : in std_logic_vector(25 downto 0);
+    valB : in ;
+    instructionIn : in std_logic_vector(31 downto 0);
+    mem_data : out std_logic_vector(31 downto 0);
+    ALU_ResultOut : out std_logic_vector(31 downto 0);
+    instructionOut : out std_logic_vector(31 downto 0);
   ) ;
 end memoryStage ;
 
