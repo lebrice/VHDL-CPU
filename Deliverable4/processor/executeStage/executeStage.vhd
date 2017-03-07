1830eb243eaa31bcd40be4345db632f272f65d03
Asher Wright <sastreen@hotmail.com>
2017-03-07 16:15:58 -0500

    Added ALU component to execute stage

--- Deliverable4/processor/executeStage/executeStage.vhd
+++ Deliverable4/processor/executeStage/executeStage.vhd
@@ -19,8 +19,26 @@ entity executeStage is
 end executeStage ;
 
 architecture executeStage_arch of executeStage is
-
+  COMPONENT ALU
+  port (
+    clock : in std_logic;
+    instruction : in INSTRUCTION;
+    op_a : in std_logic_vector(31 downto 0); -- RS
+    op_b : in std_logic_vector(31 downto 0); -- RT
+    ALU_out : out std_logic_vector(63 downto 0); -- RD
+    BranchCondition : out std_logic
+  );
+  END COMPONENT;
+  
 begin
+  --define alu component
+  exAlu: ALU port map (clock, instructionIn, , , ALU_Result, branch);
+
+
+  --here's what we need to do:
+  --1) decide between Val from memory and PC+4
+  --2) decide between Val from memory and SE
+  --3) set 1) and 2) to op_a, op_b for the ALU
   
 
 
