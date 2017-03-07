52b48006625e31a9896b16ee8b7616bf41ad10aa
Asher Wright <sastreen@hotmail.com>
2017-03-07 15:52:05 -0500

    Added execute folder & tb file

--- Deliverable4/processor/executeStage/executeStage.vhd
+++ Deliverable4/processor/executeStage/executeStage.vhd
@@ -2,15 +2,17 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
+use work.INSTRUCTION_TOOLS.all;
+
 entity executeStage is
   port (
     clock : in std_logic;
-    instructionIn : in std_logic_vector(31 downto 0);
+    instructionIn : in Instruction;
     valA : in std_logic_vector(31 downto 0);
     valB : in std_logic_vector(31 downto 0);
     iSignExtended : in std_logic_vector(31 downto 0);
     PCPlus4In : in std_logic_vector(3 downto 0);
-    instructionOut : out std_logic_vector(31 downto 0);
+    instructionOut : out Instruction;
     branch : std_logic;
     ALU_Result : std_logic_vector(31 downto 0)
   ) ;
@@ -18,10 +20,8 @@ end executeStage ;
 
 architecture executeStage_arch of executeStage is
 
-
-
 begin
-
+  
 
 
 end architecture ; -- arch
---
+++ Deliverable4/processor/executeStage/executeStage_tb.vhd
@@ -0,0 +1,12 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.numeric_std.all;
+
+ENTITY executeStage_tb IS
+END executeStage_tb;
+
+ARCHITECTURE behaviour OF executeStage_tb IS
+--components go here
+begin
+
+END;
