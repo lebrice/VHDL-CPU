d0dd387c27ba7bee53d9461eee2f974e1a5b87a2
Fabrice Normandin <fabrice.normandin@gmail.com>
2017-03-08 13:40:42 -0500

    Added First Attempt at implementation
    Added a first attempt at an implementation. It seems that there's some timing issues that arise from the "one cycle per stage" limitation. Not sure if my attempt at implementing the "reset" is quite right.
    
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- Deliverable4/processor/fetchStage/fetchStage.vhd
+++ Deliverable4/processor/fetchStage/fetchStage.vhd
@@ -17,10 +17,27 @@ entity fetchStage is
 end fetchStage;
 
 architecture fetchStage_arch of fetchStage is
+signal PC_next : integer;
+signal PC_register : integer;
+begin 
+PC <= PC_register;
+
+PC_next <= 
+  4             when reset = '1' else
+  branch_target when branch_condition = '1' else
+  PC_register   when stall = '1' else 
+  PC_register + 4;
 
 
+
+pc_process : process( clock, reset )
 begin
-
+  if( reset = '1' ) then
+    PC_register <= 0;
+  elsif( rising_edge(clock) ) then
+    PC_register <= PC_next;
+  end if ;
+end process ; -- pc_process
 
 
 end architecture ; -- arch
