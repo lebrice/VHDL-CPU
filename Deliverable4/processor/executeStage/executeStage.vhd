a5d3cda9205d09552c9e3c6b582316e917daf9fe
Asher Wright <sastreen@hotmail.com>
2017-03-07 21:08:15 -0500

    Put execute into a process
    I'm not sure if this all needs to be synced to the clock, or what. Also not sure if this will play nicely with the ALU component. It's hard when I can't test it.

--- Deliverable4/processor/executeStage/executeStage.vhd
+++ Deliverable4/processor/executeStage/executeStage.vhd
@@ -36,58 +36,62 @@ begin
   --define alu component
   exAlu: ALU port map (clock, instructionIn, input_a, input_b, ALU_Result);
 
-  -- first we will compute the "branch" output
-  case instructionIn.INSTRUCTION_TYPE is
-    when BRANCH_IF_EQUAL =>
-      if ((signed(op_a)-signed(iSignExtended)) = 0) then
-        branch <= 1;
-      else
+  computation : process( instructionIn, iSignExtended, branch, input_a, input_b)
+
+  begin
+    -- first we will compute the "branch" output
+    case instructionIn.INSTRUCTION_TYPE is
+      when BRANCH_IF_EQUAL =>
+        if ((signed(op_a)-signed(iSignExtended)) = 0) then
+          branch <= 1;
+        else
+          branch <= 0;
+        end if;
+      when BRANCH_IF_NOT_EQUAL =>
+        if ((signed(op_a)-signed(iSignExtended)) /= 0) then
+          branch <= 1;
+        else
+          branch <= 0;
+        end if;
+      when others =>
         branch <= 0;
-      end if;
-    when BRANCH_IF_NOT_EQUAL =>
-      if ((signed(op_a)-signed(iSignExtended)) /= 0) then
-        branch <= 1;
-      else
-        branch <= 0;
-      end if;
-    when others =>
-      branch <= 0;
-  end case; --TODO: figure out why there's an error here 
-  -- The instruction changes what is passed to the ALU
-  -- We either pass in:
-  --  a) values read from registers
-  --  b) shamt
-  --  c) address vector
-  --  d) immediate sign extended
-  --  e) branch target
-  case instructionIn.INSTRUCTION_FORMAT is
-    --if it's an R-type, we pass in values (unless shifting)
-    when R_TYPE =>
-      --if it is a shift, we store the shamt in "a"
-      case instructionIn.INSTRUCTION_TYPE is
-        when SHIFT_LEFT_LOGICAL | SHIFT_RIGHT_LOGICAL | SHIFT_RIGHT_ARITHMETIC =>
-          input_a <= (31 downto 5 => '0') & instructionIn.shamt_vect; --padded with 0s
-        when others =>
-          input_a <= valA;
-      end case; 
-      
-      input_b <= valB;
-    --if it's a J-type, we pass in the address vector and b value
-    when J_TYPE =>
-      input_a <= "000000" & instructionIn.address_vect;
-      input_b <= valB; --doesn't matter
-    --if it's an I-type we pass in the value A and the immediate sign extended
-    when I_TYPE =>
-      --we need to check if it's a branch (in which case we do PC + 4)
-      case instructionIn.INSTRUCTION_TYPE is
-        when BRANCH_IF_EQUAL | BRANCH_IF_NOT_EQUAL =>
-          --with branches, we want "a" to have the PCPlus4
-          input_a <= (31 downto 4 => '0') & PCPlus4In;
-        when others =>
-          input_a <= valA;   
-      end case;
-      input_b <= iSignExtended;
-    when UNKNOWN => --this is unknown. report an error.
-      report "ERROR: unknown instruction format in execute stage!" severity FAILURE;
-  end case;
+    end case; --TODO: figure out why there's an error here 
+    -- The instruction changes what is passed to the ALU
+    -- We either pass in:
+    --  a) values read from registers
+    --  b) shamt
+    --  c) address vector
+    --  d) immediate sign extended
+    --  e) branch target
+    case instructionIn.INSTRUCTION_FORMAT is
+      --if it's an R-type, we pass in values (unless shifting)
+      when R_TYPE =>
+        --if it is a shift, we store the shamt in "a"
+        case instructionIn.INSTRUCTION_TYPE is
+          when SHIFT_LEFT_LOGICAL | SHIFT_RIGHT_LOGICAL | SHIFT_RIGHT_ARITHMETIC =>
+            input_a <= (31 downto 5 => '0') & instructionIn.shamt_vect; --padded with 0s
+          when others =>
+            input_a <= valA;
+        end case; 
+        
+        input_b <= valB;
+      --if it's a J-type, we pass in the address vector and b value
+      when J_TYPE =>
+        input_a <= "000000" & instructionIn.address_vect;
+        input_b <= valB; --doesn't matter
+      --if it's an I-type we pass in the value A and the immediate sign extended
+      when I_TYPE =>
+        --we need to check if it's a branch (in which case we do PC + 4)
+        case instructionIn.INSTRUCTION_TYPE is
+          when BRANCH_IF_EQUAL | BRANCH_IF_NOT_EQUAL =>
+            --with branches, we want "a" to have the PCPlus4
+            input_a <= (31 downto 4 => '0') & PCPlus4In;
+          when others =>
+            input_a <= valA;   
+        end case;
+        input_b <= iSignExtended;
+      when UNKNOWN => --this is unknown. report an error.
+        report "ERROR: unknown instruction format in execute stage!" severity FAILURE;
+    end case;
+  end process; -- computation
 end architecture ; -- arch
