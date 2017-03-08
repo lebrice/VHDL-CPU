5c96e8f7bd56d65a873bd3f3ef5bdfb63e4a9ffd
stephanGM <stephanGM@users.noreply.github.com>
2017-03-08 18:43:34 -0500

    updated reg_file.vhd

--- Deliverable4/processor/decode/reg_file/reg_file.vhd
+++ Deliverable4/processor/decode/reg_file/reg_file.vhd
@@ -1,1 +1,24 @@
 library ieee;
+use IEEE.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+
+package REGISTER is
+
+    TYPE REGISTER_ENTRY is
+    record
+        busy : std_logic;
+        data : std_logic_vector(31 downto 0);
+    end record;
+    
+    TYPE REGISTER is array (31 downto 0) of REGISTER_ENTRY;  
+
+end REGISTER;
+
+package body REGISTER is
+
+    function set_register(instruction : std_logic_vector(31 downto 0))
+     
+    end set_register;
+
+end REGISTER;
