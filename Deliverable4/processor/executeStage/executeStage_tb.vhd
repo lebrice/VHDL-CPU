1fcb6e29c6684e89919c0332d6a201286fa6197e
Asher Wright <sastreen@hotmail.com>
2017-03-08 13:21:48 -0500

    Added clock & component to execute tb

--- Deliverable4/processor/executeStage/executeStage_tb.vhd
+++ Deliverable4/processor/executeStage/executeStage_tb.vhd
@@ -7,6 +7,16 @@ END executeStage_tb;
 
 ARCHITECTURE behaviour OF executeStage_tb IS
 --components go here
+COMPONENT execute IS
+    port (clk : in std_logic
+    );
+END COMPONENT;
 begin
-
+    clk_process : PROCESS
+    BEGIN
+        clk <= '0';
+        WAIT FOR clk_period/2;
+        clk <= '1';
+        WAIT FOR clk_period/2;
+    END PROCESS;
 END;
