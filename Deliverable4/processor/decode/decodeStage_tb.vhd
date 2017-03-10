c215e3c83c81d1ef6e8c434189494caaa3ac2d2b
Fabrice Normandin <fabrice.normandin@gmail.com>
2017-03-10 18:27:03 -0500

    Added Signals and Basic Test Process
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- Deliverable4/processor/decode/decodeStage_tb.vhd
+++ Deliverable4/processor/decode/decodeStage_tb.vhd
@@ -9,6 +9,9 @@ ENTITY decode_tb IS
 END decode_tb;
 
 architecture behaviour of decode_tb is
+
+    constant clock_period : time := 1 ns;
+
     component decodeStage is
         port (
             clock : in std_logic;
@@ -39,5 +42,61 @@ architecture behaviour of decode_tb is
         );
     end component;
 
+signal clock : std_logic;
+signal PC : integer;
+signal instruction_in : INSTRUCTION;
+signal write_back_instruction : INSTRUCTION;
+signal write_back_data : std_logic_vector(31 downto 0);
+signal val_a : std_logic_vector(31 downto 0);
+signal val_b : std_logic_vector(31 downto 0);
+signal i_sign_extended : std_logic_vector(31 downto 0);
+signal PC_out : integer;
+signal instruction_out : INSTRUCTION;
+signal register_file : REGISTER_BLOCK;
+signal stall_out : std_logic;
+
 begin
+
+    dec : decodeStage port map (
+        clock,
+        PC,
+        instruction_in,
+        write_back_instruction,
+        write_back_data,
+        val_a,val_b,
+        i_sign_extended,
+        PC_out,
+        instruction_out,
+        register_file,
+        stall_out
+    );
+
+    clk_process : process
+    BEGIN
+        clock <= '0';
+        wait for clock_period/2;
+        clock <= '1';
+        wait for clock_period/2;
+    end process;
+
+    test_process : process
+    variable registers : register_block;
+    begin
+        -- TODO: figure out if the return value really needs to be used or not.
+        registers := reset_register_block(registers);
+
+        for I in 0 to NUM_REGISTERS loop
+            registers(I).data := std_logic_vector(to_unsigned(I, 32));
+            registers(I).busy := '0';
+        end loop;
+
+        PC <= 0;
+        instruction_in <= makeInstruction(ALU_OP, 1,2,3,0, ADD_FN); -- ADD R1 R2 R3
+
+
+        wait;
+
+    end process;
+
+
 end behaviour;
