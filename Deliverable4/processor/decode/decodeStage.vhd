8e8c12bf0bf53715aca721449a30f13eba7bcc6d
Fabrice Normandin <fabrice.normandin@gmail.com>
2017-03-10 15:36:15 -0500

    Modified Decode Stage Entity Declaration
    - Added uses of the INSTRUCTION and REGISTER_BLOCK types
    - Changed PC to Integer type
    - changed the Write_Back input to the INSTRUCTION type, instead of a simple index and data. (For later, with data hasards, we'll have more control)
    
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- Deliverable4/processor/decode/decodeStage.vhd
+++ Deliverable4/processor/decode/decodeStage.vhd
@@ -2,18 +2,35 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
+use work.INSTRUCTION_TOOLS.all;
+use work.REGISTERS.all;
+
 entity decodeStage is
   port (
     clock : in std_logic;
-    PCPlus4In : in std_logic_vector(3 downto 0);
-    writeData : in std_logic_vector(31 downto 0);
-    writeRegister : in std_logic_vector(31 downto 0);
-    instructionIn : in std_logic_vector(31 downto 0);
-    valA : out std_logic_vector(31 downto 0);
-    valB : out std_logic_vector(31 downto 0);
-    iSignExtended : out std_logic_vector(31 downto 0);
-    PCPlus4Out : out std_logic_vector(3 downto 0);
-    instructionOut : out std_logic_vector(31 downto 0)
+
+    -- Inputs coming from the IF/ID Register
+    PC : in integer;
+    instruction_in : in INSTRUCTION;
+
+
+    -- Instruction and data coming from the Write-Back stage.
+    write_back_instruction : in INSTRUCTION;
+    write_back_data : in std_logic_vector(31 downto 0);
+
+
+    -- Outputs to the ID/EX Register
+    val_a : out std_logic_vector(31 downto 0);
+    val_b : out std_logic_vector(31 downto 0);
+    i_sign_extended : out std_logic_vector(31 downto 0);
+    PC_out : out integer;
+    instruction_out : out INSTRUCTION;
+
+    -- Register file
+    register_file : in REGISTER_BLOCK;
+
+    -- Stall signal out.
+    stall_out : out std_logic
     
   ) ;
 end decodeStage ;
