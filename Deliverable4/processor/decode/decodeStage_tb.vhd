1d02569574eddec4a7143e8f0f35920795529f8a
Fabrice Normandin <fabrice.normandin@gmail.com>
2017-03-10 17:58:32 -0500

    Added a decodeStage Component
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- Deliverable4/processor/decode/decodeStage_tb.vhd
+++ Deliverable4/processor/decode/decodeStage_tb.vhd
@@ -3,6 +3,41 @@ USE ieee.std_logic_1164.all;
 USE ieee.numeric_std.all;
 
 USE work.INSTRUCTION_TOOLS.all;
+USE work.registers.all;
 
 ENTITY decode_tb IS
 END decode_tb;
+
+architecture behaviour of decode_tb is
+    component decodeStage is
+        port (
+            clock : in std_logic;
+
+            -- Inputs coming from the IF/ID Register
+            PC : in integer;
+            instruction_in : in INSTRUCTION;
+
+
+            -- Instruction and data coming from the Write-Back stage.
+            write_back_instruction : in INSTRUCTION;
+            write_back_data : in std_logic_vector(31 downto 0);
+
+
+            -- Outputs to the ID/EX Register
+            val_a : out std_logic_vector(31 downto 0);
+            val_b : out std_logic_vector(31 downto 0);
+            i_sign_extended : out std_logic_vector(31 downto 0);
+            PC_out : out integer;
+            instruction_out : out INSTRUCTION;
+
+            -- Register file
+            register_file : in REGISTER_BLOCK;
+
+            -- Stall signal out.
+            stall_out : out std_logic
+            
+        );
+    end component;
+
+begin
+end behaviour;
