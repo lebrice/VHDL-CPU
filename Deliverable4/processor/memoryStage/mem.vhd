28b8e422bf90e78fc697239e6d258b8aca6a409f
WPoole <wpoole94@gmail.com>
2017-03-11 23:37:29 -0500

    Fixed casing of vars in mem

--- Deliverable4/processor/memoryStage/mem.vhd
+++ Deliverable4/processor/memoryStage/mem.vhd
@@ -5,13 +5,13 @@ USE ieee.numeric_std.all;
 ENTITY mem is
     port (
         clock : in std_logic;
-        ALU_ResultIn : in std_logic_vector(31 downto 0);
-        ALU_ResultOut : out std_logic_vector(31 downto 0);
-        instructionIn : in std_logic_vector(31 downto 0);
-        instructionOut : out std_logic_vector(31 downto 0);
-        branchTakenIn : in  std_logic;
-        branchTakenOut : out  std_logic;
-        valB : in std_logic_vector(31 downto 0);
+        ALU_result_in : in std_logic_vector(31 downto 0);
+        ALU_result_out : out std_logic_vector(31 downto 0);
+        instruction_in : in std_logic_vector(31 downto 0);
+        instruction_out : out std_logic_vector(31 downto 0);
+        branch_taken_in : in  std_logic;
+        branch_taken_out : out  std_logic;
+        val_b : in std_logic_vector(31 downto 0);
         mem_data : out std_logic_vector(31 downto 0)
     );
 END mem;
@@ -21,17 +21,15 @@ BEGIN
 
     mem_process : PROCESS ()
     BEGIN
-        IF(instructionIn is branch and branchTakenIn == '1') THEN
-            branchTakenOut = '1';
-        ELSIF(instructionIn is load) THEN
-            mem_data <= DATACACHE[ALU_ResultIn];
-        ELSIF(instructionIn is store) THEN
-            DATACACHE[ALU_ResultIn] <= valB;
+        IF(instruction_in is branch and branch_taken_in == '1') THEN
+            branch_taken_out = '1';
+        ELSIF(instruction_in is load) THEN
+            mem_data <= DATACACHE[ALU_result_in];
+        ELSIF(instruction_in is store) THEN
+            DATACACHE[ALU_result_in] <= valB;
         END IF;
 
-        instructionOut <= instructionIn;
-        ALU_ResultOut <= ALU_ResultIn;
-
-    END PROCESS;
-
-END memArch;
+        instresult_iut <= instruction_in;
+        ALU_result_out <= ALU_result_in;_i
+    END PRO_tE_t_o_i_o
+E_bD memArch;
