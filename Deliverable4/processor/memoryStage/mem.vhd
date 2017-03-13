4232215ad3fff862e3fda0f90e34e5f85aaee463
WPoole <wpoole94@gmail.com>
2017-03-13 13:36:54 -0400

    Added Avalon interface stuff for data memory.

--- Deliverable4/processor/memoryStage/mem.vhd
+++ Deliverable4/processor/memoryStage/mem.vhd
@@ -13,14 +13,22 @@ ENTITY mem is
         branch_taken_in : in  std_logic;
         branch_taken_out : out  std_logic;
         val_b : in std_logic_vector(31 downto 0);
-        mem_data : out std_logic_vector(31 downto 0)
+        mem_data : out std_logic_vector(31 downto 0);
+
+        m_addr : out integer range 0 to ram_size-1;
+        m_read : out std_logic;
+        m_readdata : in std_logic_vector (bit_width-1 downto 0);        
+        m_writedata : out std_logic_vector (bit_width-1 downto 0);
+        m_write : out std_logic;
+        m_waitrequest : in std_logic -- Unused until the Avalon Interface is added.
+
     );
 END mem;
 
 ARCHITECTURE memArch OF mem IS
 BEGIN
 
-    mem_process : process(clock)
+    mem_process : process(clock, m_waitrequest)
     BEGIN
         CASE instruction_in.INSTRUCTION_TYPE IS
             WHEN branch_if_equal | branch_if_not_equal =>
@@ -28,9 +36,16 @@ BEGIN
                     branch_taken_out <= '1';
                 END IF;
             WHEN load_word =>
-                mem_data <= DATACACHE[ALU_result_in]; --TODO: Call memory correctly
+                -- TODO: add the proper timing and avalon interface stuff later.
+                m_read <= '1';
+                m_addr <= ALU_result_in;
+                mem_data <= m_readdata;
             WHEN store_word =>
-                DATACACHE[ALU_result_in] <= valB; --TODO: Call memory correctly
+                -- TODO: add the proper timing and avalon interface stuff later.
+                m_write <= '1';
+                m_addr <= ALU_result_in;
+                m_writedata <= val_b;
+                
         END CASE;
         instruction_out <= instruction_in;
         ALU_result_out <= ALU_result_in;
