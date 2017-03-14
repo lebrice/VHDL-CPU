05685af9a54386bbaf56f9b03cfa3346dea66f6a
Asher Wright <sastreen@hotmail.com>
2017-03-13 20:26:24 -0400

    working on multiplication alu

--- Deliverable4/processor/alu.vhd
+++ Deliverable4/processor/alu.vhd
@@ -27,6 +27,7 @@ begin
   computation : process( instruction_type, op_a, op_b )
   variable a : signed(31 downto 0) := signed(op_a);
   variable b : signed(31 downto 0) := signed(op_b);
+  variable c : signed(61 downto 0);
   --shamt is stored in last 5 bits of "a"
   variable shift_amount : integer := to_integer(unsigned(op_a(4 downto 0))); --TODO: find out if this is unsigned or signed... (shamt is positive, I think)
 
@@ -44,7 +45,8 @@ begin
       when SUBTRACT =>
         ALU_out <= std_logic_vector(a - b); -- rs - rt
       when MULTIPLY =>
-        ALU_out <= std_logic_vector(a * b)(31 downto 0);
+        c <= a * b;
+        ALU_out <= std_logic_vector(c(31 downto 0));
       
       when DIVIDE =>
         ALU_out <= std_logic_vector(a / b);
