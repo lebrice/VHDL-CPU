fea37852502b6f62622c0abcb64e8efcc62e9cc8
Asher Wright <sastreen@hotmail.com>
2017-03-15 16:32:03 -0400

    Changed ALU to 64 bit output

--- Deliverable4/processor/alu.vhd
+++ Deliverable4/processor/alu.vhd
@@ -10,12 +10,16 @@ entity ALU is
     instruction_type : in INSTRUCTION_TYPE;
     op_a : in std_logic_vector(31 downto 0); -- RS
     op_b : in std_logic_vector(31 downto 0); -- RT
-    ALU_out : out std_logic_vector(31 downto 0) -- RD
+    ALU_out : out std_logic_vector(63 downto 0) -- RD
   );
 end ALU ;
 
 architecture ALU_arch of ALU is
-
+  function extend64(value : std_logic_vector(31 downto 0))
+    return std_logic_vector is
+  begin
+      return X"00000000" & value;
+  end extend64;
   -- Implements assembly of a limited set of 32 instructions:
   -- R-Instructions: mult, mflo, jr, mfhi, add, sub, and, div, slt, or, nor, xor, sra, srl, sll;
   -- I-Instructions: addi, slti, bne, sw, beq, lw, lb, sb, lui, andi, ori, xori, asrt, asrti, halt;
@@ -41,35 +45,35 @@ begin
         --for load word, provide the target address, (R[rs] + SignExtendedImmediate).
 
         -- for branch if equal PC = PC + 4 + branch target
-          -- TODO: Assuming that the Branch target is calculated with A being the current PC + 4.
-        ALU_out <= std_logic_vector(a + b); 
+        -- TODO: Assuming that the Branch target is calculated with A being the current PC + 4.
+        ALU_out <= extend64(std_logic_vector(a + b)); 
       
       when SUBTRACT =>
-        ALU_out <= std_logic_vector(a - b); -- rs - rt
+        ALU_out <= extend64(std_logic_vector(a - b)); -- rs - rt
       when MULTIPLY =>
         ALU_out <= std_logic_vector(a*b); --TODO: figure out bit issue
       
       when DIVIDE =>
-        ALU_out <= std_logic_vector(a / b);
+        ALU_out <= extend64(std_logic_vector(a / b));
       
       when SET_LESS_THAN | SET_LESS_THAN_IMMEDIATE =>
         if a < b then  -- if rs < rd
-          ALU_out <= x"00000001";
+          ALU_out <= X"00000000" & x"00000001";
         else 
-          ALU_out <= x"00000000";
+          ALU_out <= X"00000000" & x"00000000";
         end if;  
       
       when BITWISE_AND | BITWISE_AND_IMMEDIATE=>
-        ALU_out <= op_a AND op_b;
+        ALU_out <= extend64(op_a AND op_b);
       
       when BITWISE_OR | BITWISE_OR_IMMEDIATE =>
-        ALU_out <= op_a OR op_b;
+        ALU_out <= extend64(op_a OR op_b);
       
       when BITWISE_NOR =>
-        ALU_out <= op_a NOR op_b;
+        ALU_out <= extend64(op_a NOR op_b);
       
       when BITWISE_XOR | BITWISE_XOR_IMMEDIATE =>
-        ALU_out <= op_a XOR op_b;
+        ALU_out <= extend64(op_a XOR op_b);
       
       when MOVE_FROM_HI =>
         -- This case is never reached (handled in decode)
@@ -81,20 +85,20 @@ begin
 
       when LOAD_UPPER_IMMEDIATE =>
         -- loads the upper 16 bits of RT with the 16 bit immediate, and all the lower bits to '0'.
-        ALU_out <= op_b(31 downto 16) & X"0000";
+        ALU_out <= extend64(op_b(31 downto 16) & X"0000");
       
       when SHIFT_LEFT_LOGICAL =>
-        ALU_out <= std_logic_vector(b SLL shift_amount); 
+        ALU_out <= extend64(std_logic_vector(b SLL shift_amount)); 
       
       when SHIFT_RIGHT_LOGICAL =>
-        ALU_out <= std_logic_vector(b SRL shift_amount);
+        ALU_out <= extend64(std_logic_vector(b SRL shift_amount));
       
       when SHIFT_RIGHT_ARITHMETIC =>
-        ALU_out <= to_stdlogicvector(to_bitvector(op_b) sra shift_amount);      
+        ALU_out <= extend64(to_stdlogicvector(to_bitvector(op_b) sra shift_amount));
       
       when JUMP | JUMP_AND_LINK | JUMP_TO_REGISTER =>
         --assumes the correctly formatted new address is set to a.
-        ALU_out <= op_a;
+        ALU_out <= extend64(op_a);
 
       when UNKNOWN =>
         report "ERROR: unknown instruction given to ALU!" severity FAILURE;
