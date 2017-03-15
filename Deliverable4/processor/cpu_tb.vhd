3cb4124c6be8ced1051b70ac132a4644789a0c3d
Fabrice Normandin <fabrice.normandin@gmail.com>
2017-03-15 17:28:02 -0400

    Added CPU Testbench File
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

---
+++ Deliverable4/processor/cpu_tb.vhd
@@ -0,0 +1,49 @@
+library ieee ;
+    use ieee.std_logic_1164.all ;
+    use ieee.numeric_std.all ;
+
+use work.INSTRUCTION_TOOLS.all;
+use work.REGISTERS.all;
+
+entity cpu_tb is
+end cpu_tb ; 
+
+architecture processor_test of cpu_tb is
+    COMPONENT CPU is
+        generic(
+            ram_size : integer := 8196;
+            bit_width : integer := 32
+        );
+        port (
+            clock : in std_logic;
+            initialize : in std_logic -- signals to load Instruciton and Data Memories. Should be held at '1' for at least a few clock cycles.
+        );
+    end COMPONENT;
+
+    signal clock : std_logic := '0';
+    signal initialize : std_logic := '0';
+
+    constant clock_period : time := 1 ns;
+begin
+
+c1 : CPU PORT MAP (
+    clock,
+    initialize
+);
+
+
+clock_process : process
+begin
+    clock <= '0';
+    wait for clock_period/2;
+    clock <= '1';
+    wait for clock_period/2;
+end process ; -- clock_process
+
+
+test_process : process ( clock )
+begin
+
+end process test_process;
+
+end architecture ;
