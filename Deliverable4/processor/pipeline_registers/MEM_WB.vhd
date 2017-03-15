ebff2ca2c0f840003dd677a14db5233d7d398590
Asher Wright <sastreen@hotmail.com>
2017-03-15 18:49:50 -0400

    Removed PC from MEM_WB

--- Deliverable4/processor/pipeline_registers/MEM_WB.vhd
+++ Deliverable4/processor/pipeline_registers/MEM_WB.vhd
@@ -6,8 +6,6 @@ USE work.instruction_tools.all;
 ENTITY mem_wb_entity IS
 	PORT (
         clock: IN STD_LOGIC;
-        pc_in: IN INTEGER;
-        pc_out: OUT INTEGER;
         instruction_in: IN INSTRUCTION;
         instruction_out: OUT INSTRUCTION;
         alu_result_in: IN STD_LOGIC_VECTOR(63 DOWNTO 0);
@@ -18,7 +16,6 @@ ENTITY mem_wb_entity IS
 END mem_wb_entity;
 
 ARCHITECTURE mem_wb_architecture OF mem_wb_entity IS
-    SIGNAL pc_intermediate: INTEGER;
     SIGNAL instruction_intermediate: INSTRUCTION;
     SIGNAL alu_result_intermediate: STD_LOGIC_VECTOR(63 DOWNTO 0);
     SIGNAL data_mem_intermediate: STD_LOGIC_VECTOR(31 DOWNTO 0);
@@ -27,8 +24,6 @@ BEGIN
     mem_wb_process: PROCESS (clock)
     BEGIN
         IF(clock'EVENT AND clock = '1') THEN
-            pc_intermediate <= pc_in;
-            pc_out <= pc_intermediate;
 
             instruction_intermediate <= instruction_in;
             instruction_out <= instruction_intermediate;
