570bfa1e6fab9cd31f3cec4909d32a5634d16c57
Asher Wright <sastreen@hotmail.com>
2017-03-14 21:01:02 -0400

    Created skeleton for CPU

---
+++ Deliverable4/processor/cpu.vhd
@@ -0,0 +1,21 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+-- opcode tool library
+use work.INSTRUCTION_TOOLS.all;
+
+
+--entity declaration
+entity CPU is
+  port (
+    clock : in std_logic
+  );
+end CPU ;
+
+
+architecture CPU_arch of ALU is
+
+begin
+
+end architecture;
