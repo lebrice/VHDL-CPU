fe721de2de45f1ded7907cb57e1d93dd1726eec7
Asher Wright <sastreen@hotmail.com>
2017-03-14 21:03:58 -0400

    Added fetch stage component to cpu

--- Deliverable4/processor/cpu.vhd
+++ Deliverable4/processor/cpu.vhd
@@ -15,7 +15,25 @@ end CPU ;
 
 
 architecture CPU_arch of ALU is
-
+   COMPONENT fetchStage IS
+        PORT (
+            clock : in std_logic;
+            reset : in std_logic;
+            branch_target : in integer;
+            branch_condition : in std_logic;
+            stall : in std_logic;
+            instruction_out : out Instruction;
+            PC : out integer;
+            m_addr : out integer;
+            m_read : out std_logic;
+            m_readdata : in std_logic_vector (bit_width-1 downto 0);
+            -- m_write : out std_logic;
+            -- m_writedata : out std_logic_vector (bit_width-1 downto 0);
+            m_waitrequest : in std_logic -- unused until the Avalon Interface is added.
+        );
+    END COMPONENT;
+    
+    
 begin
 
 end architecture;
