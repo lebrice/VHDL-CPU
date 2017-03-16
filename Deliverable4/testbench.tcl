c185db460742f1dc2159970667771cf6c3bfe510
Fabrice Normandin <fabrice.normandin@gmail.com>
2017-03-16 14:36:25 -0400

    Fixed Paths in testbench.tcl
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- Deliverable4/testbench.tcl
+++ Deliverable4/testbench.tcl
@@ -1,21 +1,21 @@
 vlib work
 
 ;# Compile the needed components
-vcom instruction.vhd
-vcom memory.vhd
-vcom writebackStage.vhd
-vcom alu.vhd
-vcom executeStage.vhd
-vcom reg_file.vhd
-vcom decodeStage.vhd
-vcom fetchStage.vhd
-vcom EX_MEM.vhd
-vcom ID_EX.vhd
-vcom IF_ID.vhd
-vcom MEM_WB.vhd
-vcom memoryStage.vhd
-vcom cpu.vhd
-vcom cpu_tb.vhd
+vcom processor/instruction/instruction.vhd
+vcom processor/memory/memory.vhd
+vcom processor/writeback/writebackStage.vhd
+vcom processor/alu.vhd
+vcom processor/executeStage/executeStage.vhd
+vcom processor/decode/reg_file/reg_file.vhd
+vcom processor/decode/decodeStage.vhd
+vcom processor/fetchStage/fetchStage.vhd
+vcom processor/pipeline_registers/EX_MEM.vhd
+vcom processor/pipeline_registers/ID_EX.vhd
+vcom processor/pipeline_registers/IF_ID.vhd
+vcom processor/pipeline_registers/MEM_WB.vhd
+vcom processor/memoryStage/memoryStage.vhd
+vcom processor/cpu.vhd
+vcom processor/cpu_tb.vhd
 
 ;# Start simulation
 vsim cpu_tb
