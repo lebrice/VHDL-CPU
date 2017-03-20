04692d94a7cb2ebff20cb6107d1f5a6b1eaf9091
Asher Wright <sastreen@hotmail.com>
2017-03-19 23:35:43 -0400

    Changed testbench to all work with files in folder

--- Deliverable4/testbench.tcl
+++ Deliverable4/testbench.tcl
@@ -1,21 +1,21 @@
 vlib work
 
 ;# Compile the needed components
-vcom processor/instruction/instruction.vhd
-vcom processor/memory/memory.vhd
-vcom processor/writebackStage/writebackStage.vhd
-vcom processor/executeStage/alu.vhd
-vcom processor/executeStage/executeStage.vhd
-vcom processor/decodeStage/reg_file/reg_file.vhd
-vcom processor/decodeStage/decodeStage.vhd
-vcom processor/fetchStage/fetchStage.vhd
-vcom processor/pipeline_registers/EX_MEM.vhd
-vcom processor/pipeline_registers/ID_EX.vhd
-vcom processor/pipeline_registers/IF_ID.vhd
-vcom processor/pipeline_registers/MEM_WB.vhd
-vcom processor/memoryStage/memoryStage.vhd
-vcom processor/cpu.vhd
-vcom processor/cpu_tb.vhd
+vcom instruction.vhd
+vcom memory.vhd
+vcom writebackStage.vhd
+vcom alu.vhd
+vcom executeStage.vhd
+vcom reg_file/reg_file.vhd
+vcom decodeStage.vhd
+vcom fetchStage.vhd
+vcom EX_MEM.vhd
+vcom ID_EX.vhd
+vcom IF_ID.vhd
+vcom MEM_WB.vhd
+vcom memoryStage.vhd
+vcom cpu.vhd
+vcom cpu_tb.vhd
 
 ;# Start simulation
 vsim cpu_tb
