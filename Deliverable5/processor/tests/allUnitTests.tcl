115d5614534fd5b5f05c298cf3e4607ab30d6ed0
Asher Wright <sastreen@hotmail.com>
2017-03-25 21:46:20 -0400

    Added missing directory info for tests

--- Deliverable5/processor/tests/allUnitTests.tcl
+++ Deliverable5/processor/tests/allUnitTests.tcl
@@ -2,35 +2,29 @@ vlib work
 
 ;# Compile the needed components
 
-vcom instruction/instruction.vhd
-vcom instruction/instruction_tb.vhd
-vcom fetchStage/fetchStage.vhd
-vcom fetchStage/fetchStage_tb.vhd
-
-vcom decodeStage/decodeStage.vhd
-vcom decodeStage/decodeStage_tb.vhd
-vcom decodeStage/reg_file/reg_file.vhd
-vcom decodeStage/reg_file/reg_file_tb.vhd
-
-vcom executeStage/alu.vhd
-vcom executeStage/executeStage.vhd
-vcom executeStage/executeStage_tb.vhd
-
-vcom pipeline_registers/EX_MEM.vhd
-vcom pipeline_registers/ID_EX.vhd
-vcom pipeline_registers/IF_ID.vhd
-vcom pipeline_registers/MEM_WB.vhd
-
-vcom memory/memory.vhd
-vcom memory/memory_tb.vhd
-vcom memoryStage/memoryStage.vhd
-vcom memoryStage/memoryStage_tb.vhd
-
-vcom writebackStage/writebackStage.vhd
-vcom writebackStage/writebackStage_tb.vhd
-
-vcom cpu.vhd
-vcom cpu_tb.vhd
+vcom ../instruction/instruction.vhd
+vcom ../instruction/instruction_tb.vhd
+vcom ../fetchStage/fetchStage.vhd
+vcom ../fetchStage/fetchStage_tb.vhd
+vcom ../decodeStage/decodeStage.vhd
+vcom ../decodeStage/decodeStage_tb.vhd
+vcom ../decodeStage/reg_file/reg_file.vhd
+vcom ../decodeStage/reg_file/reg_file_tb.vhd
+vcom ../executeStage/alu.vhd
+vcom ../executeStage/executeStage.vhd
+vcom ../executeStage/executeStage_tb.vhd
+vcom ../pipeline_registers/EX_MEM.vhd
+vcom ../pipeline_registers/ID_EX.vhd
+vcom ../pipeline_registers/IF_ID.vhd
+vcom ../pipeline_registers/MEM_WB.vhd
+vcom ../memory/memory.vhd
+vcom ../memory/memory_tb.vhd
+vcom ../memoryStage/memoryStage.vhd
+vcom ../memoryStage/memoryStage_tb.vhd
+vcom ../writebackStage/writebackStage.vhd
+vcom ../writebackStage/writebackStage_tb.vhd
+vcom ../cpu.vhd
+vcom ../cpu_tb.vhd
 
 vcom tests/addi_tb.vhd
 vcom tests/add_tb.vhd
