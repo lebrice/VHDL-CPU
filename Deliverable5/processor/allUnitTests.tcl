10e0bf59ba22523f3e44bfad8e1b073ebf7ace5e
Fabrice Normandin <fabrice.normandin@gmail.com>
2017-03-27 19:53:16 -0400

    Improved TCL File, Added All TestBenches, Quiet
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- Deliverable5/processor/allUnitTests.tcl
+++ Deliverable5/processor/allUnitTests.tcl
@@ -1,64 +1,104 @@
 vlib work
 
 ;# Compile the needed components
-vcom decodeStage/reg_file/reg_file.vhd
-vcom decodeStage/reg_file/reg_file_tb.vhd
-vcom instruction/instruction.vhd
-vcom instruction/instruction_tb.vhd
-vcom fetchStage/fetchStage.vhd
-vcom fetchStage/fetchStage_tb.vhd
-vcom decodeStage/decodeStage.vhd
-vcom decodeStage/decodeStage_tb.vhd
-vcom executeStage/alu.vhd
-vcom executeStage/executeStage.vhd
-vcom executeStage/executeStage_tb.vhd
-vcom pipeline_registers/EX_MEM.vhd
-vcom pipeline_registers/ID_EX.vhd
-vcom pipeline_registers/IF_ID.vhd
-vcom pipeline_registers/MEM_WB.vhd
-vcom memory/memory.vhd
-vcom memory/memory_tb.vhd
-vcom memoryStage/memoryStage.vhd
-vcom memoryStage/memoryStage_tb.vhd
-vcom writebackStage/writebackStage.vhd
-vcom writebackStage/writebackStage_tb.vhd
-vcom cpu.vhd
-vcom cpu_tb.vhd
+vcom decodeStage/reg_file/reg_file.vhd -quiet
+vcom decodeStage/reg_file/reg_file_tb.vhd -quiet
+vcom instruction/instruction.vhd -quiet
+vcom instruction/instruction_tb.vhd -quiet
+vcom branch_management.vhd -quiet
+vcom fetchStage/fetchStage.vhd -quiet
+vcom fetchStage/fetchStage_tb.vhd -quiet
+vcom decodeStage/decodeStage.vhd -quiet
+vcom decodeStage/decodeStage_tb.vhd -quiet
+vcom executeStage/alu.vhd -quiet
+vcom executeStage/executeStage.vhd -quiet
+vcom executeStage/executeStage_tb.vhd -quiet
+vcom pipeline_registers/EX_MEM.vhd -quiet
+vcom pipeline_registers/ID_EX.vhd -quiet
+vcom pipeline_registers/IF_ID.vhd -quiet
+vcom pipeline_registers/MEM_WB.vhd -quiet
+vcom memory/memory.vhd -quiet
+vcom memory/memory_tb.vhd -quiet
+vcom memoryStage/memoryStage.vhd -quiet
+vcom memoryStage/memoryStage_tb.vhd -quiet
+vcom writebackStage/writebackStage.vhd -quiet
+vcom writebackStage/writebackStage_tb.vhd -quiet
+vcom cpu.vhd -quiet
+vcom cpu_tb.vhd -quiet
 
-vcom tests/addi_tb.vhd
-vcom tests/add_tb.vhd
-vcom tests/beq_tb.vhd
-vcom tests/bne_tb.vhd
-vcom tests/j_tb.vhd
-vcom tests/jal_tb.vhd
-vcom tests/jr_tb.vhd
-vcom tests/lw_tb.vhd
-vcom tests/sll_tb.vhd
-vcom tests/sra_tb.vhd
-vcom tests/srl_tb.vhd
+vcom tests/add_tb.vhd -quiet
+vcom tests/sub_tb.vhd -quiet
+vcom tests/addi_tb.vhd -quiet
+vcom tests/mult_tb.vhd -quiet
+vcom tests/div_tb.vhd -quiet
+vcom tests/slt_tb.vhd -quiet
+vcom tests/slti_tb.vhd -quiet
+vcom tests/and_tb.vhd -quiet
+vcom tests/or_tb.vhd -quiet
+vcom tests/nor_tb.vhd -quiet
+vcom tests/xor_tb.vhd -quiet
+vcom tests/andi_tb.vhd -quiet
+vcom tests/ori_tb.vhd -quiet
+vcom tests/xori_tb.vhd -quiet
+vcom tests/lui_tb.vhd -quiet
+vcom tests/sll_tb.vhd -quiet
+vcom tests/srl_tb.vhd -quiet
+vcom tests/sra_tb.vhd -quiet
+vcom tests/lw_tb.vhd -quiet
+vcom tests/beq_tb.vhd -quiet
+vcom tests/bne_tb.vhd -quiet
+vcom tests/j_tb.vhd -quiet
+vcom tests/jr_tb.vhd -quiet
+vcom tests/jal_tb.vhd -quiet
+vcom 
 
 
 ;# Start simulation and run each tb
-vsim addi_tb
+vsim add_tb -quiet
 run 1000ns
-vsim add_tb
+vsim sub_tb -quiet
 run 1000ns
-vsim beq_tb
+vsim addi_tb -quiet
 run 1000ns
-vsim bne_tb
+vsim mult_tb -quiet
 run 1000ns
-vsim j_tb
+vsim div_tb -quiet
 run 1000ns
-vsim jal_tb
+vsim slt_tb -quiet
 run 1000ns
-vsim jr_tb
+vsim slti_tb -quiet
 run 1000ns
-vsim lw_tb
+vsim and_tb -quiet
 run 1000ns
-vsim sll_tb
+vsim or_tb -quiet
 run 1000ns
-vsim sra_tb
+vsim nor_tb -quiet
 run 1000ns
-vsim srl_tb
+vsim xor_tb -quiet
 run 1000ns
-
+vsim andi_tb -quiet
+run 1000ns
+vsim ori_tb -quiet
+run 1000ns
+vsim xori_tb -quiet
+run 1000ns
+vsim lui_tb -quiet
+run 1000ns
+vsim sll_tb -quiet
+run 1000ns
+vsim srl_tb -quiet
+run 1000ns
+vsim sra_tb -quiet
+run 1000ns
+vsim lw_tb -quiet
+run 1000ns
+vsim beq_tb -quiet
+run 1000ns
+vsim bne_tb -quiet
+run 1000ns
+vsim j_tb -quiet
+run 1000ns
+vsim jr_tb -quiet
+run 1000ns
+vsim jal_tb -quiet
+run 1000ns
