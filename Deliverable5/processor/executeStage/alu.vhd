9735dc7cf09f47e885d468c77eda235734ec4b3c
Fabrice Normandin <fabrice.normandin@gmail.com>
2017-04-07 15:47:39 -0400

    Removed Branch Logic from ALU
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- Deliverable5/processor/executeStage/alu.vhd
+++ Deliverable5/processor/executeStage/alu.vhd
@@ -34,13 +34,6 @@ begin
   --shamt is stored in last 5 bits of "a"
   variable shift_amount : integer; 
 
-  variable pc_int : integer := 0;
-  variable address_offset : integer := 0;
-  variable new_address : integer := 0;
-
-  variable highest_bits : std_logic_vector(3 downto 0);
-  variable lowest_bits :  std_logic_vector(27 downto 0);
-  variable jump_address : std_logic_vector(31 downto 0);
   begin
     --set initial values
     a := signed(op_a);
@@ -53,15 +46,7 @@ begin
         --for load word, provide the target address, (R[rs] + SignExtendedImmediate).
         -- for branch if equal PC = PC + 4 + branch target
         ALU_out <= extend64(std_logic_vector(a + b)); 
-        
-      when BRANCH_IF_EQUAL | BRANCH_IF_NOT_EQUAL =>
-        --b is our PC
-        pc_int := to_integer(unsigned(op_a));
-        address_offset := to_integer(b sll 2);
-        new_address := pc_int + 4 + address_offset;
-        ALU_out(31 downto 0) <= std_logic_vector(to_unsigned(new_address, 32));
-        ALU_out(63 downto 32) <= (others => '0');
-      
+
       when SUBTRACT =>
         ALU_out <= extend64(std_logic_vector(a - b)); -- rs - rt
 
@@ -118,15 +103,8 @@ begin
       when SHIFT_RIGHT_ARITHMETIC =>
         ALU_out <= extend64(to_stdlogicvector(to_bitvector(op_b) sra shift_amount));
       
-      when JUMP | JUMP_AND_LINK =>
-        --assumes the PC is in a, and address vector is in b.
-        highest_bits := op_a(31 downto 28);
-        lowest_bits := op_b(25 downto 0) & "00";
-        jump_address := highest_bits & lowest_bits;
-        ALU_out <= extend64(jump_address);
-      
-      when JUMP_TO_REGISTER =>
-        ALU_out <= extend64(op_a);
+      when BRANCH_IF_EQUAL | BRANCH_IF_NOT_EQUAL | JUMP | JUMP_AND_LINK | JUMP_TO_REGISTER =>
+        -- This logic was moved to the ID stage to allow predict-taken branch prediction to work without the need for a stall cycle.
 
       when UNKNOWN =>
         report "ERROR: unknown instruction given to ALU!" severity FAILURE;
