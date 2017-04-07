1656b67ae85fc0e292ea3c4617a979cdb3631b1d
stephanGM <stephanGM@users.noreply.github.com>
2017-04-06 20:18:40 -0400

    wrote should_take_branch() logic

--- Deliverable5/processor/branch_management.vhd
+++ Deliverable5/processor/branch_management.vhd
@@ -47,16 +47,44 @@ package body BRANCH_MANAGEMENT is
     -- Function responsible for making an assumption about the taken/not taken behaviour, for branch prediction.
     function should_take_branch(fetch_stage_pc : Integer ; branch_buff: branch_buffer)
         return boolean is
+        variable ones : Integer;
+        variable zeros: Integer;
     begin
+        ones := 0;
+        -- parse through branch buffer entries
         for i in branch_buff' range loop
+
+            -- fetch pc matches one in buffer
             if (branch_buff(i).pc = fetch_stage_pc) then
-                -- access: branch_buff(i).taken to evaluate
-                -- do some magic i.e. look at the string of taken bits and decide
-                -- if some condition on taken bits : return true;
-            else
-                null;
-            end if;
-        end loop;
+
+                -- count the number of ones in the in the taken history of identified PC
+                for j in branch_buff(i).taken' range loop     
+                    if branch_buff(i).taken(j) = '1' then
+                        ones := ones + 1; 
+                    end if;
+                end loop;
+
+                -- determine if the number of 1s if greater than number of 0s
+                zeros := N_BIT_PREDICTION - ones;
+
+                -- act
+                if (ones > zeros) then      -- more branches, predict taken
+                    return true;
+                elsif (ones < zeros)then    -- less branches, assume not taken
+                    return false;
+                elsif(ones = zeros) then    -- equal taken and not taken
+                    
+                    -- check to see which happened last
+                    if (branch_buff(i).taken(0) = '1') then -- branch was taken last
+                        return true;    -- assume it will be taken again
+                    else
+                        return false;  -- branch was not taken last, assume not taken
+                    end if;
+
+                end if;  -- act 
+            end if; -- fetch pc matches one in buffer
+        end loop; -- parse through branch buffer entries
+
         return false;   -- default return is false for prediction not taken
     end should_take_branch;
 
