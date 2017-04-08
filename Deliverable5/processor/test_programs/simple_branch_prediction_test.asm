3c913db62350271a8af4272f1b1f31ab9b293867
Fabrice Normandin <fabrice.normandin@gmail.com>
2017-04-08 19:44:45 -0400

    Updated Test Program to help with Testing
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- Deliverable5/processor/test_programs/simple_branch_prediction_test.asm
+++ Deliverable5/processor/test_programs/simple_branch_prediction_test.asm
@@ -3,9 +3,15 @@
 
 	addi $1, $0, 10	
 	addi $2, $0, 15
-	bneq  $1, $2, END
+	bne  $1, $2, END
 	addi $1, $0, 30
 	addi $2, $0, 35
+	addi $3, $0, 10
+	addi $4, $0, 10
+	addi $5, $0, 10
+	addi $6, $0, 10
+	addi $7, $0, 10
+	addi $8, $0, 10
 
 END:	sw $1, 0($0)
 		sw $2, 4($0)
